ad2cdba818d49000439eb8103184b7b4351ddfda
loganhuss4317 <loganhuss@gmail.com>
2025-10-02 13:20:12 -0400

    render json as HTML

--- index.js
+++ index.js
@@ -3,18 +3,21 @@
 async function main() {
 const searchMovie = await fetch("http://www.omdbapi.com/?s=fast%26furious&apikey=f5814458")
 const movieData = await searchMovie.json();
-console.log(
-movieData.map(
-    (movie) => `<div class="search-card">
-        <div class="search__container">
-            <h3>Movie</h3>
-            <p><b>Title</b> Title</p>
-            <p><b>Year</b> Year</p>
-            <p><b>imdbID</b> imdbID</p>
-            <p><b>Poster</b> Poster</p>
-        </div>
-</div>`)
-);
+const searchList = document.querySelector('.search-list');
+console.log(movieData)
+searchList.innerHTML = 
+    movieData.Search.map(
+    (search) => 
+        `<div class="search-card">
+            <div class="search__container">
+                <h3>Movie</h3>
+                <p><b>${search.Title}</b></p>
+                <p><b>Year:</b> ${search.Year}</p>
+                <p><b>imdbID:</b> ${search.imdbID}</p>
+                <p><b>Poster:</b> ${search.Poster}</p>
+            </div>
+        </div>`
+    ).join("")
 }
 
 main();
