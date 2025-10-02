f89774f1201cdfd9d32f5d2621c536d73013b12a
loganhuss4317 <loganhuss@gmail.com>
2025-10-02 09:39:59 -0400

    js update

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-// API http://www.omdbapi.com/?t=fast&apikey=f5814458
+// API http://www.omdbapi.com/?s=fast%26furious&apikey=f5814458
 
-console.log("http://www.omdbapi.com/?t=fast&apikey=f5814458")
+async function main() {
+const searchMovie = await fetch("http://www.omdbapi.com/?s=fast%26furious&apikey=f5814458")
+const movieData = await searchMovie.json();
+console.log(
+movieData.map(
+    (movie) => `<div class="search-card">
+        <div class="search__container">
+            <h3>Movie</h3>
+            <p><b>Title</b> Title</p>
+            <p><b>Year</b> Year</p>
+            <p><b>imdbID</b> imdbID</p>
+            <p><b>Poster</b> Poster</p>
+        </div>
+</div>`)
+);
+}
+
+main();
