b08b220f54f5aee0d81b4efcf6849fd30cc2ad68
loganhuss4317 <loganhuss@gmail.com>
2025-10-06 11:42:56 -0400

    js updates

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ async function main() {
 const searchMovie = await fetch("http://www.omdbapi.com/?s=fast%26furious&apikey=f5814458")
 const movieData = await searchMovie.json();
 const searchList = document.querySelector('.search-list');
-console.log(movieData)
 searchList.innerHTML = 
     movieData.Search.map((search) => searchHTML(search)).join("")
 }
 
 main();
 
+function showMovies(Title) {
+console.log(Title);                                                                                                                                                        1
+}
+
 function searchHTML(search) {
-    return `<div class="search-card">
+    return `<div class="search-card" onsearch="showMovies(${search.Title})">
             <div class="search__container">
                 <h3>Movie</h3>
                 <p><b>${search.Title}</b></p>
